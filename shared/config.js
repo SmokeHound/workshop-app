e975b1273b747bcf39c7483f954200fed310fb22
SmokeHound <23314326+SmokeHound@users.noreply.github.com>
2025-09-09 14:04:33 +1000

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,7 +1,7 @@
 // shared/config.js
 
 // API Endpoints
-export const API_BASE_URL = 'https://workshop-backend.joshburt.com.au';
+export const API_BASE_URL = 'https://workshop-backend.joshburt.com.au/api';
 
 // Role Constants
 export const ROLES = {
